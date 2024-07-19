4ced37bf3e6397acfb013b1327ec1e727825efb6
deilann <yandoryn@gmail.com>
2024-07-19 13:08:33 -0600

    created into_screen.tscn
    Created an intro screen scene to start the game.

---
+++ Timer.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class Timer : Godot.Timer
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		GD.Randomize();
+	}
+	private void _on_timeout()
+	{
+		Label text = GetNode<Label>("Text");
+		int increase = Convert.ToInt16(GD.Randi() % 6);
+		text.VisibleCharacters += increase;
+
+		if (text.VisibleRatio >= 1)
+		{
+			TextureButton start = GetNode<TextureButton>("Start");
+			start.Visible = true;
+			Stop();
+		}
+	}
+}
