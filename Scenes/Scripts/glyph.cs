e0fb48f62d12312e7f67793e4152edb888472fce
deilann <yandoryn@gmail.com>
2024-07-19 16:08:14 -0600

    add intro scene to main

---
+++ Scenes/Scripts/glyph.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class glyph : Node2D
+{
+	[ExportGroup("Transform")] 
+	[Export] 
+	public Texture2D GlyphIcon;
+	[Export]
+	public int ScaleNode = 1;
+	
+	[ExportGroup("Visibility")]
+	[Export]
+	public bool SubMenuVisible { get; set; }
+	[Export]
+	public bool FlowVisible { get; set; }
+	[Export]
+	public bool AscendVisible { get; set; }
+	[Export]
+	public bool DescendVisible { get; set; }
+	
+	[ExportGroup("Label")]
+	[Export]
+	public string LabelText { get; set; }
+	
+	
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
