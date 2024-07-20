0d63fb74efb6feb241f3f78953347ac14a364b27
deilann <yandoryn@gmail.com>
2024-07-19 18:32:41 -0600

    FIX casing conflict

--- Scenes/Scripts/Glyph.cs
+++ Scenes/Scripts/Glyph.cs
@@ -5,36 +5,36 @@ namespace BrokenCircle.Scenes.Scripts;
 [Tool]
 public partial class Glyph : Node2D
 {
-	[Export] 
-	public Texture2D GlyphIcon;
-	[Export] public int ScaleNode = 1;
-	[Export] public bool SubMenuVisible = false;
-	[Export] public bool FlowVisible = false;
-	[Export] public bool AscendVisible = false;
-	[Export] public bool DescendVisible = false;
-	[Export]
-	public string LabelText { get; set; }
+    [Export] 
+    public Texture2D GlyphIcon;
+    [Export] public int ScaleNode = 1;
+    [Export] public bool SubMenuVisible = false;
+    [Export] public bool FlowVisible = false;
+    [Export] public bool AscendVisible = false;
+    [Export] public bool DescendVisible = false;
+    [Export]
+    public string LabelText { get; set; }
 	
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		TextureRect glyphRect =
-			GetNode<TextureRect>("ControlCanvas/ButtonCanvas/GlyphButtonCanvas/GlyphButton/GlyphTexture");
-		glyphRect.Texture = GlyphIcon;
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        TextureRect glyphRect =
+            GetNode<TextureRect>("ControlCanvas/ButtonCanvas/GlyphButtonCanvas/GlyphButton/GlyphTexture");
+        glyphRect.Texture = GlyphIcon;
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
 		
-		Control subMenu = GetNode<Control>("ControlCanvas/ButtonCanvas/SubMenuCanvas");
-		subMenu.Visible = SubMenuVisible;
-		Control flowButtonCanvas = GetNode<Control>("ControlCanvas/ButtonCanvas/SubMenuCanvas/FlowButtonCanvas");
-		flowButtonCanvas.Visible = FlowVisible;
-		Control ascendButtonCanvas = GetNode<Control>("ControlCanvas/ButtonCanvas/SubMenuCanvas/AscendButtonCanvas");
-		ascendButtonCanvas.Visible = AscendVisible;
-		Control descendButtonCanvas = GetNode<Control>("ControlCanvas/ButtonCanvas/SubMenuCanvas/DescendButtonCanvas");
-		descendButtonCanvas.Visible = DescendVisible;
+        Control subMenu = GetNode<Control>("ControlCanvas/ButtonCanvas/SubMenuCanvas");
+        subMenu.Visible = SubMenuVisible;
+        Control flowButtonCanvas = GetNode<Control>("ControlCanvas/ButtonCanvas/SubMenuCanvas/FlowButtonCanvas");
+        flowButtonCanvas.Visible = FlowVisible;
+        Control ascendButtonCanvas = GetNode<Control>("ControlCanvas/ButtonCanvas/SubMenuCanvas/AscendButtonCanvas");
+        ascendButtonCanvas.Visible = AscendVisible;
+        Control descendButtonCanvas = GetNode<Control>("ControlCanvas/ButtonCanvas/SubMenuCanvas/DescendButtonCanvas");
+        descendButtonCanvas.Visible = DescendVisible;
 		
-	}
+    }
 }
