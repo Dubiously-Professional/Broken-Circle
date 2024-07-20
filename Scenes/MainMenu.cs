a1216b37b11226fc5c8f3539d21a310f2cc9da70
deilann <yandoryn@gmail.com>
2024-07-20 15:15:59 -0600

    Update MainMenu.cs to include Flow state setting

--- Scenes/MainMenu.cs
+++ Scenes/MainMenu.cs
@@ -14,6 +14,7 @@ public partial class MainMenu : Node2D
         glyph0001Self.ActionButton = false;
         glyph0001Self.AscendVisible = false;
         glyph0001Self.DescendVisible = true;
+        glyph0001Self.FlowVisible = false;
         glyph0001Self.SubMenuVisible = false;
         
         Glyph glyph0011Know = GetNode<Glyph>("CanvasGroup/Glyph0011Know/Glyph");
@@ -21,6 +22,7 @@ public partial class MainMenu : Node2D
         glyph0011Know.ActionButton = false;
         glyph0011Know.AscendVisible = false;
         glyph0011Know.DescendVisible = true;
+        glyph0001Self.FlowVisible = false;
         glyph0011Know.SubMenuVisible = false;
         
         Glyph glyph0012Electricity = GetNode<Glyph>("CanvasGroup/Glyph0012Electricity/Glyph");
@@ -28,6 +30,7 @@ public partial class MainMenu : Node2D
         glyph0012Electricity.ActionButton = false;
         glyph0012Electricity.AscendVisible = false;
         glyph0012Electricity.DescendVisible = true;
+        glyph0001Self.FlowVisible = false;
         glyph0012Electricity.SubMenuVisible = false;
         
         Glyph glyph0013Force = GetNode<Glyph>("CanvasGroup/Glyph0013Force/Glyph");
@@ -35,6 +38,7 @@ public partial class MainMenu : Node2D
         glyph0013Force.ActionButton = false;
         glyph0013Force.AscendVisible = false;
         glyph0013Force.DescendVisible = true;
+        glyph0001Self.FlowVisible = false;
         glyph0013Force.SubMenuVisible = false;
         
         Glyph glyph0021Move = GetNode<Glyph>("CanvasGroup/Glyph0021Move/Glyph");
@@ -42,6 +46,7 @@ public partial class MainMenu : Node2D
         glyph0021Move.ActionButton = false;
         glyph0021Move.AscendVisible = false;
         glyph0021Move.DescendVisible = true;
+        glyph0001Self.FlowVisible = false;
         glyph0021Move.SubMenuVisible = false;
         
         Glyph glyph0022Gas = GetNode<Glyph>("CanvasGroup/Glyph0022Gas/Glyph");
@@ -49,6 +54,7 @@ public partial class MainMenu : Node2D
         glyph0022Gas.ActionButton = false;
         glyph0022Gas.AscendVisible = false;
         glyph0022Gas.DescendVisible = true;
+        glyph0001Self.FlowVisible = false;
         glyph0022Gas.SubMenuVisible = false;
         
         Glyph glyph0023Emotion = GetNode<Glyph>("CanvasGroup/Glyph0023Emotion/Glyph");
@@ -56,6 +62,7 @@ public partial class MainMenu : Node2D
         glyph0023Emotion.ActionButton = false;
         glyph0023Emotion.AscendVisible = false;
         glyph0023Emotion.DescendVisible = true;
+        glyph0001Self.FlowVisible = false;
         glyph0023Emotion.SubMenuVisible = false;
         
         Glyph glyph0030Light = GetNode<Glyph>("CanvasGroup/Glyph0030Light/Glyph");
@@ -63,6 +70,7 @@ public partial class MainMenu : Node2D
         glyph0030Light.ActionButton = false;
         glyph0030Light.AscendVisible = false;
         glyph0030Light.DescendVisible = true;
+        glyph0001Self.FlowVisible = false;
         glyph0030Light.SubMenuVisible = false;
         
         Glyph glyph0031Math = GetNode<Glyph>("CanvasGroup/Glyph0031Math/Glyph");
@@ -70,6 +78,7 @@ public partial class MainMenu : Node2D
         glyph0031Math.ActionButton = false;
         glyph0031Math.AscendVisible = false;
         glyph0031Math.DescendVisible = true;
+        glyph0001Self.FlowVisible = false;
         glyph0031Math.SubMenuVisible = false;
         
     }
