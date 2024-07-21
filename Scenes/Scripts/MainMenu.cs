bd1e2f5982822641f9af5fd9ff79c18d79fc4dd1
Chris <christopher.b.delgesso@gmail.com>
2024-07-21 16:57:57 -0400

    Sending

--- Scenes/Scripts/MainMenu.cs
+++ Scenes/Scripts/MainMenu.cs
@@ -4,31 +4,31 @@ using BrokenCircle.Scenes.Scripts;
 
 public partial class MainMenu : Node2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        // Set initial Glyph states
-        // @TODO: make this a full method of its own
-        
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Set initial Glyph states
+		// @TODO: make this a full method of its own
+		
 
-    }
+	}
 
-    private void ShowCloseButton()
-    {
-        CanvasItem start = GetNode<CanvasItem>("TypingText/MessageAlert/CloseButton");
-        start.Visible = true;
-        AudioStreamPlayer2D player = MainScreen.Instance.GetNode<AudioStreamPlayer2D>("Sounds/ButtonAppear");
-        player.Play();
-    }
+	private void ShowCloseButton()
+	{
+		CanvasItem start = GetNode<CanvasItem>("TypingText/MessageAlert/CloseButton");
+		start.Visible = true;
+		AudioStreamPlayer2D player = MainScreen.Instance.GetNode<AudioStreamPlayer2D>("Sounds/ButtonAppear");
+		player.Play();
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 
-    private void DescendElectricity() {
-        MainScreen.Instance.ScreenContents =
-            ResourceLoader.Load<PackedScene>("res://Scenes/ElectricityDisplay.tscn").Instantiate();
-    }
+	private void DescendElectricity() {
+		MainScreen.Instance.ScreenContents =
+			ResourceLoader.Load<PackedScene>("res://Scenes/ElectricityDisplay.tscn").Instantiate();
+	}
 
 }
