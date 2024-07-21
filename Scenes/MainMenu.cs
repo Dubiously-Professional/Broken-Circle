5fd47df35295de0689f07b080e5e03ff2c8c42a5
Chris <christopher.b.delgesso@gmail.com>
2024-07-21 12:07:29 -0400

    pulling grief, so depositing the status scene. Ignore the mods made to the scripts, all it did was add whitespace to the end

--- Scenes/MainMenu.cs
+++ Scenes/MainMenu.cs
@@ -6,82 +6,82 @@ public partial class MainMenu : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-    {
-        // Set initial Glyph states
-        // @TODO: make this a full method of its own
-        Glyph glyph0001Self = GetNode<Glyph>("CanvasGroup/Glyph0001Self/Glyph");
-        glyph0001Self.ScaleNode = 1;
-        glyph0001Self.ActionButton = false;
-        glyph0001Self.AscendVisible = false;
-        glyph0001Self.DescendVisible = true;
-        glyph0001Self.FlowVisible = false;
-        glyph0001Self.SubMenuVisible = false;
-        
-        Glyph glyph0011Know = GetNode<Glyph>("CanvasGroup/Glyph0011Know/Glyph");
-        glyph0011Know.ScaleNode = 1;
-        glyph0011Know.ActionButton = false;
-        glyph0011Know.AscendVisible = false;
-        glyph0011Know.DescendVisible = true;
-        glyph0001Self.FlowVisible = false;
-        glyph0011Know.SubMenuVisible = false;
-        
-        Glyph glyph0012Electricity = GetNode<Glyph>("CanvasGroup/Glyph0012Electricity/Glyph");
-        glyph0012Electricity.ScaleNode = 1;
-        glyph0012Electricity.ActionButton = false;
-        glyph0012Electricity.AscendVisible = false;
-        glyph0012Electricity.DescendVisible = true;
-        glyph0001Self.FlowVisible = false;
-        glyph0012Electricity.SubMenuVisible = false;
-        
-        Glyph glyph0013Force = GetNode<Glyph>("CanvasGroup/Glyph0013Force/Glyph");
-        glyph0013Force.ScaleNode = 1;
-        glyph0013Force.ActionButton = false;
-        glyph0013Force.AscendVisible = false;
-        glyph0013Force.DescendVisible = true;
-        glyph0001Self.FlowVisible = false;
-        glyph0013Force.SubMenuVisible = false;
-        
-        Glyph glyph0021Move = GetNode<Glyph>("CanvasGroup/Glyph0021Move/Glyph");
-        glyph0021Move.ScaleNode = 1;
-        glyph0021Move.ActionButton = false;
-        glyph0021Move.AscendVisible = false;
-        glyph0021Move.DescendVisible = true;
-        glyph0001Self.FlowVisible = false;
-        glyph0021Move.SubMenuVisible = false;
-        
-        Glyph glyph0022Gas = GetNode<Glyph>("CanvasGroup/Glyph0022Gas/Glyph");
-        glyph0022Gas.ScaleNode = 1;
-        glyph0022Gas.ActionButton = false;
-        glyph0022Gas.AscendVisible = false;
-        glyph0022Gas.DescendVisible = true;
-        glyph0001Self.FlowVisible = false;
-        glyph0022Gas.SubMenuVisible = false;
-        
-        Glyph glyph0023Emotion = GetNode<Glyph>("CanvasGroup/Glyph0023Emotion/Glyph");
-        glyph0023Emotion.ScaleNode = 1;
-        glyph0023Emotion.ActionButton = false;
-        glyph0023Emotion.AscendVisible = false;
-        glyph0023Emotion.DescendVisible = true;
-        glyph0001Self.FlowVisible = false;
-        glyph0023Emotion.SubMenuVisible = false;
-        
-        Glyph glyph0030Light = GetNode<Glyph>("CanvasGroup/Glyph0030Light/Glyph");
-        glyph0030Light.ScaleNode = 1;
-        glyph0030Light.ActionButton = false;
-        glyph0030Light.AscendVisible = false;
-        glyph0030Light.DescendVisible = true;
-        glyph0001Self.FlowVisible = false;
-        glyph0030Light.SubMenuVisible = false;
-        
-        Glyph glyph0031Math = GetNode<Glyph>("CanvasGroup/Glyph0031Math/Glyph");
-        glyph0031Math.ScaleNode = 1;
-        glyph0031Math.ActionButton = false;
-        glyph0031Math.AscendVisible = false;
-        glyph0031Math.DescendVisible = true;
-        glyph0001Self.FlowVisible = false;
-        glyph0031Math.SubMenuVisible = false;
-        
-    }
+	{
+		// Set initial Glyph states
+		// @TODO: make this a full method of its own
+		Glyph glyph0001Self = GetNode<Glyph>("CanvasGroup/Glyph0001Self/Glyph");
+		glyph0001Self.ScaleNode = 1;
+		glyph0001Self.ActionButton = false;
+		glyph0001Self.AscendVisible = false;
+		glyph0001Self.DescendVisible = true;
+		glyph0001Self.FlowVisible = false;
+		glyph0001Self.SubMenuVisible = false;
+		
+		Glyph glyph0011Know = GetNode<Glyph>("CanvasGroup/Glyph0011Know/Glyph");
+		glyph0011Know.ScaleNode = 1;
+		glyph0011Know.ActionButton = false;
+		glyph0011Know.AscendVisible = false;
+		glyph0011Know.DescendVisible = true;
+		glyph0001Self.FlowVisible = false;
+		glyph0011Know.SubMenuVisible = false;
+		
+		Glyph glyph0012Electricity = GetNode<Glyph>("CanvasGroup/Glyph0012Electricity/Glyph");
+		glyph0012Electricity.ScaleNode = 1;
+		glyph0012Electricity.ActionButton = false;
+		glyph0012Electricity.AscendVisible = false;
+		glyph0012Electricity.DescendVisible = true;
+		glyph0001Self.FlowVisible = false;
+		glyph0012Electricity.SubMenuVisible = false;
+		
+		Glyph glyph0013Force = GetNode<Glyph>("CanvasGroup/Glyph0013Force/Glyph");
+		glyph0013Force.ScaleNode = 1;
+		glyph0013Force.ActionButton = false;
+		glyph0013Force.AscendVisible = false;
+		glyph0013Force.DescendVisible = true;
+		glyph0001Self.FlowVisible = false;
+		glyph0013Force.SubMenuVisible = false;
+		
+		Glyph glyph0021Move = GetNode<Glyph>("CanvasGroup/Glyph0021Move/Glyph");
+		glyph0021Move.ScaleNode = 1;
+		glyph0021Move.ActionButton = false;
+		glyph0021Move.AscendVisible = false;
+		glyph0021Move.DescendVisible = true;
+		glyph0001Self.FlowVisible = false;
+		glyph0021Move.SubMenuVisible = false;
+		
+		Glyph glyph0022Gas = GetNode<Glyph>("CanvasGroup/Glyph0022Gas/Glyph");
+		glyph0022Gas.ScaleNode = 1;
+		glyph0022Gas.ActionButton = false;
+		glyph0022Gas.AscendVisible = false;
+		glyph0022Gas.DescendVisible = true;
+		glyph0001Self.FlowVisible = false;
+		glyph0022Gas.SubMenuVisible = false;
+		
+		Glyph glyph0023Emotion = GetNode<Glyph>("CanvasGroup/Glyph0023Emotion/Glyph");
+		glyph0023Emotion.ScaleNode = 1;
+		glyph0023Emotion.ActionButton = false;
+		glyph0023Emotion.AscendVisible = false;
+		glyph0023Emotion.DescendVisible = true;
+		glyph0001Self.FlowVisible = false;
+		glyph0023Emotion.SubMenuVisible = false;
+		
+		Glyph glyph0030Light = GetNode<Glyph>("CanvasGroup/Glyph0030Light/Glyph");
+		glyph0030Light.ScaleNode = 1;
+		glyph0030Light.ActionButton = false;
+		glyph0030Light.AscendVisible = false;
+		glyph0030Light.DescendVisible = true;
+		glyph0001Self.FlowVisible = false;
+		glyph0030Light.SubMenuVisible = false;
+		
+		Glyph glyph0031Math = GetNode<Glyph>("CanvasGroup/Glyph0031Math/Glyph");
+		glyph0031Math.ScaleNode = 1;
+		glyph0031Math.ActionButton = false;
+		glyph0031Math.AscendVisible = false;
+		glyph0031Math.DescendVisible = true;
+		glyph0001Self.FlowVisible = false;
+		glyph0031Math.SubMenuVisible = false;
+		
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
--- Scenes/Scripts/Glyphs.cs
+++ Scenes/Scripts/Glyphs.cs
@@ -9,102 +9,102 @@ namespace BrokenCircle.Scenes.Scripts;
 [Tool]
 public partial class Glyphs : Node
 {
-    [Signal]
-    public delegate void GlyphTranslationChangedEventHandler(GlyphType glyphType, string translation);
+	[Signal]
+	public delegate void GlyphTranslationChangedEventHandler(GlyphType glyphType, string translation);
 
-    public enum GlyphType
-    {
-        Flow0000,
-        Self0001,
-        Contain0002,
-        Affirm0003,
-        Safe0010,
-        One0011,
-        Know0012,
-        Electricity0013,
-        Force0020,
-        Move0021,
-        Gas0022,
-        Emotion0023,
-        Light0030,
-        Math0031,
-        Not0032,
-        Origin0033,
-        Place0100,
-        Object0101,
-        Metal0102,
-        On0103,
-        Cut0110
-    };
+	public enum GlyphType
+	{
+		Flow0000,
+		Self0001,
+		Contain0002,
+		Affirm0003,
+		Safe0010,
+		One0011,
+		Know0012,
+		Electricity0013,
+		Force0020,
+		Move0021,
+		Gas0022,
+		Emotion0023,
+		Light0030,
+		Math0031,
+		Not0032,
+		Origin0033,
+		Place0100,
+		Object0101,
+		Metal0102,
+		On0103,
+		Cut0110
+	};
 
-    public string[,] TranslationOptions = {
-        { "????", "FLOW", "FLOW", "FLOW", "FLOW" },
-        { "????", "SELF", "SELF", "SELF", "SELF" },
-        { "????", "CONTAIN", "OTHER", "MOVE", "TIME" },
-        { "????", "AFFIRM", "AFFIRM", "AFFIRM", "AFFIRM" },
-        { "????", "EMOTION", "SAFE", "FORCE", "OBJECT" },
-        { "????", "EMPTY", "ONE", "TWO", "THREE" },
-        { "????", "KNOW", "OBJECT", "NOT", "MATH" },
-        { "????", "FORCE", "SAFE", "POWER", "CUT" },
-        { "????", "FORCE", "METAL", "CUT", "OBJECT" },
-        { "????", "ORIGIN", "MOVE", "PLACE", "MOVE" },
-        { "????", "OBJECT", "GAS", "LIQUID", "METAL" },
-        { "????", "EMOTION", "OBJECT", "ART", "OTHER" },
-        { "????", "ART", "AFFIRM", "LIGHT", "OTHER" },
-        { "????", "EMPTY", "ONE", "MATH", "ART" },
-        { "????", "NOT", "TIME", "AFTER", "BEFORE" },
-        { "????", "PLACE", "OTHER", "MOVE", "ORIGIN" },
-        { "????", "LIGHT", "MOVE", "PLACE", "FORCE" },
-        { "????", "EMPTY", "ONE", "MATH", "OBJECT" },
-        { "????", "OBJECT", "GAS", "LIQUID", "METAL" },
-        { "????", "ON", "TIME", "EMPTY", "BEFORE" },
-        { "????", "GAS", "AFTER", "CUT", "METAL"}
-    };
-    
-    
-    private Texture2D[] _glyphIcons;
-    private string[] _glyphTranslations;
-    private string[][] _glyphTranslationOptions;
+	public string[,] TranslationOptions = {
+		{ "????", "FLOW", "FLOW", "FLOW", "FLOW" },
+		{ "????", "SELF", "SELF", "SELF", "SELF" },
+		{ "????", "CONTAIN", "OTHER", "MOVE", "TIME" },
+		{ "????", "AFFIRM", "AFFIRM", "AFFIRM", "AFFIRM" },
+		{ "????", "EMOTION", "SAFE", "FORCE", "OBJECT" },
+		{ "????", "EMPTY", "ONE", "TWO", "THREE" },
+		{ "????", "KNOW", "OBJECT", "NOT", "MATH" },
+		{ "????", "FORCE", "SAFE", "POWER", "CUT" },
+		{ "????", "FORCE", "METAL", "CUT", "OBJECT" },
+		{ "????", "ORIGIN", "MOVE", "PLACE", "MOVE" },
+		{ "????", "OBJECT", "GAS", "LIQUID", "METAL" },
+		{ "????", "EMOTION", "OBJECT", "ART", "OTHER" },
+		{ "????", "ART", "AFFIRM", "LIGHT", "OTHER" },
+		{ "????", "EMPTY", "ONE", "MATH", "ART" },
+		{ "????", "NOT", "TIME", "AFTER", "BEFORE" },
+		{ "????", "PLACE", "OTHER", "MOVE", "ORIGIN" },
+		{ "????", "LIGHT", "MOVE", "PLACE", "FORCE" },
+		{ "????", "EMPTY", "ONE", "MATH", "OBJECT" },
+		{ "????", "OBJECT", "GAS", "LIQUID", "METAL" },
+		{ "????", "ON", "TIME", "EMPTY", "BEFORE" },
+		{ "????", "GAS", "AFTER", "CUT", "METAL"}
+	};
+	
+	
+	private Texture2D[] _glyphIcons;
+	private string[] _glyphTranslations;
+	private string[][] _glyphTranslationOptions;
 
 
-    public Glyphs() {
-        if (Instance != null) {
-            QueueFree();
-            return;
-        }
+	public Glyphs() {
+		if (Instance != null) {
+			QueueFree();
+			return;
+		}
 
-        Instance = this;
+		Instance = this;
 
-        string[] glyphNames = Enum.GetNames<GlyphType>();
-        _glyphIcons = new Texture2D[glyphNames.Length];
-        _glyphTranslations = new string[glyphNames.Length];
-        _glyphTranslationOptions = new string[glyphNames.Length][];
+		string[] glyphNames = Enum.GetNames<GlyphType>();
+		_glyphIcons = new Texture2D[glyphNames.Length];
+		_glyphTranslations = new string[glyphNames.Length];
+		_glyphTranslationOptions = new string[glyphNames.Length][];
 
-        for (int i = 0; i < glyphNames.Length; i++) {
-            _glyphIcons[i] = GD.Load<Texture2D>("res://Assets/Sprites/Ideograms/" + glyphNames[i] + ".png");
-            _glyphTranslations[i] = "????";
-            // TODO
-            _glyphTranslationOptions[i] = new []{TranslationOptions[i,0], TranslationOptions[i,1], TranslationOptions[i,2], TranslationOptions[i,3], TranslationOptions[i,4]}
-            ;
-        }
-    }
+		for (int i = 0; i < glyphNames.Length; i++) {
+			_glyphIcons[i] = GD.Load<Texture2D>("res://Assets/Sprites/Ideograms/" + glyphNames[i] + ".png");
+			_glyphTranslations[i] = "????";
+			// TODO
+			_glyphTranslationOptions[i] = new []{TranslationOptions[i,0], TranslationOptions[i,1], TranslationOptions[i,2], TranslationOptions[i,3], TranslationOptions[i,4]}
+			;
+		}
+	}
 
-    public static Glyphs Instance { get; private set; }
+	public static Glyphs Instance { get; private set; }
 
-    public Texture2D GetGlyphIcon(GlyphType glyphType) {
-        return _glyphIcons[(int)glyphType];
-    }
+	public Texture2D GetGlyphIcon(GlyphType glyphType) {
+		return _glyphIcons[(int)glyphType];
+	}
 
-    public string GetGlyphTranslation(GlyphType glyphType) {
-        return _glyphTranslations[(int)glyphType];
-    }
+	public string GetGlyphTranslation(GlyphType glyphType) {
+		return _glyphTranslations[(int)glyphType];
+	}
 
-    public void SetGlyphTranslation(GlyphType glyphType, string translation) {
-        _glyphTranslations[(int)glyphType] = translation;
-        EmitSignal(SignalName.GlyphTranslationChanged, (int)glyphType, translation);
-    }
+	public void SetGlyphTranslation(GlyphType glyphType, string translation) {
+		_glyphTranslations[(int)glyphType] = translation;
+		EmitSignal(SignalName.GlyphTranslationChanged, (int)glyphType, translation);
+	}
 
-    public string[] GetGlyphTranslationOptions(GlyphType glyphType) {
-        return _glyphTranslationOptions[(int)glyphType];
-    }
+	public string[] GetGlyphTranslationOptions(GlyphType glyphType) {
+		return _glyphTranslationOptions[(int)glyphType];
+	}
 }
